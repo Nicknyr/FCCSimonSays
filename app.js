645094c8fa918cea03756d4d4017f758255c8063
Nick Kinlen <nick_kinlen@hotmail.com>
2017-09-16 05:01:06 -0400

    Initial algorithm/functions layed out

--- app.js
+++ app.js
@@ -1,27 +1,71 @@
 /*
-1. Start() function starts game on the click of the start button
-2. NextLevel() function selects random buttons at an increasingly difficult level
+1. start() function starts game on the click of the start button
+2. nextLevel() function selects random buttons at an increasingly difficult level
 3. storeClicks() function stores user's pattern of clicks in array
-4. Validate() function checks if the user advances to next level or fails
-5. If user fails the GameOver function ends the game and returns game state to original page
+4. validate() function checks if the user advances to next level or fails
+5. If user fails the gameOver() function ends the game and returns game state to original page
+6. If it's the last level and player wins call win() function
 
 */
 
 
-var i = [];
+var playerMoves = [];
+var aiMoves = [];
+var aiTurn = true;
+var playerTurn = false;
 
 $(document).ready(function(){
+
+  start();
+
+  function start(){
+    $('.switch').click(function(){
+      nextLevel();
+    })
+  }
+
+  function nextLevel(){
+    // Logic for AI's turn goes here, call storeClicks function and make it the user's turn
+
+    // Number of moves in aiMoves[] increases by 1, push additional element to array
+
+    // After AI goes it's the player's turn
+    aiTurn = false;
+    playerTurn = true;
+    storeClicks();
+  }
 
   function storeClicks(){
    $('.button').each(function(elem) {
      $(this).click(function(e) {
-      i.push($(this).val());
-        console.log(i);
+      playerMoves.push($(this).val());
+        console.log(playerMoves);
+
+        // Player has made their moves, validate their moves
+          validate(playerMoves);
       });
     });
   }
 
-  storeClicks();
+  function validate(playerMoves){
+    // If player's moves are the same as AI moves player advances to next level, call nextLevel
+    nextLevel();
+    aiTurn = true;
+    playerTurn = false;
+      // If it's the last level and player wins call win() function
+      win();
+    // If player's moves are wrong player loses, call gameOver()
+    gameOver();
+    aiTurn = true;
+    playerTurn = false;
+  }
+
+  function win(){
+    gameOver();
+    alert('You win!');
+    aiTurn = true;
+    playerTurn = false;
+  }
 
 
 
@@ -29,6 +73,8 @@ $(document).ready(function(){
 
 
 
+
+// Sounds  ********************************************************************/
   function sound1(){
     var audio = new Audio();
     audio.src = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3";
